d8c5f9eb90d1adf4c3a52516ab54895975ab1176
raheemadamboev <vividricemaryandmax@gmail.com>
2022-05-18 18:31:12 +0500

    inject named dependencies to activity, prepare activity for scope

--- app/src/main/java/xyz/teamgravity/koindemo/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/koindemo/MainActivity.kt
@@ -3,11 +3,21 @@ package xyz.teamgravity.koindemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import org.koin.android.ext.android.inject
+import org.koin.android.scope.AndroidScopeComponent
+import org.koin.androidx.scope.activityScope
 import org.koin.androidx.viewmodel.ext.android.getViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import org.koin.core.qualifier.named
+import org.koin.core.scope.Scope
 import xyz.teamgravity.koindemo.ui.theme.KoinDemoTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity(), AndroidScopeComponent {
+
+    override val scope: Scope by activityScope()
+
+    private val strong by inject<String>(named(Power.Strong))
+    private val goat by inject<String>(named(Power.Goat))
 
     private val activityViewModel by viewModel<MainViewModel>()
 
@@ -15,6 +25,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         println("raheem: activityViewModel.onGetRonaldo(): ${activityViewModel.onGetRonaldo()}")
+
+        println("raheem: strong: $strong")
+        println("raheem: goat: $goat")
 
         setContent {
             KoinDemoTheme {
