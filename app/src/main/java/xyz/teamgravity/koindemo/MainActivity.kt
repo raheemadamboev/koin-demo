0e46f63d4135c0d3bbafeefed7e5585a7dd63b8c
raheemadamboev <vividricemaryandmax@gmail.com>
2022-05-18 18:23:56 +0500

    inject viewmodel to activity

--- app/src/main/java/xyz/teamgravity/koindemo/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/koindemo/MainActivity.kt
@@ -3,12 +3,19 @@ package xyz.teamgravity.koindemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import org.koin.androidx.compose.getViewModel
+import org.koin.androidx.viewmodel.ext.android.getViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 import xyz.teamgravity.koindemo.ui.theme.KoinDemoTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val activityViewModel by viewModel<MainViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        println("raheem: activityViewModel.onGetRonaldo(): ${activityViewModel.onGetRonaldo()}")
+
         setContent {
             KoinDemoTheme {
                 val viewmodel = getViewModel<MainViewModel>()
